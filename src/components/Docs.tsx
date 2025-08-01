0f651f05e195a4ebc76301724b1f47648500f4fd
enderdev-v <endercrackgamer@outlook.com>
2025-08-01 15:04:03 -0600

    feat: Docs Button

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
+import { DefaultDocs } from "../constants/constants";
+import Links from "./Links";
 
-export default function Docs({}) {
-    const [state, setState] = useState(true);
-    const [docs, addDocs] = useState([]);
+export default function Docs({ }) {
+  const [state, setState] = useState(true);
+  const [docs, addDocs] = useState([]);
 
-    
-
-    const DropdownClass = state ? "hidden" : "block";
-    return (
-        <div className="relative left-0.5 top-0 w-60 rounded-2xl m-4 select-none">
-          <div className="flex items-center dark:bg-gray-900 bg-gray-300 dark:text-white text-gray-900  px-3 py-3 rounded-lg cursor-pointer hover:outline-indigo-700 hover:outline-1" onClick={() => setState(!state)}>
-            <span className="mr-2"><i className='bx bxs-book-open'></i>&nbsp;Docs</span>
-            <ul className={`absolute top-full left-0 mt-1 dark:bg-gray-900 dark:text-white text-gray-800 bg-gray-300 rounded-lg p-2 w-full ${DropdownClass} z-50 shadow-lg`}>
-              
-            </ul>
-          </div>
-        </div>
-    );
+  const DropdownClass = state ? "hidden" : "block";
+  return (
+    <div className="relative left-0.5 top-0 w-60 rounded-2xl m-4 select-none">
+      <div className="flex items-center dark:bg-gray-900 bg-gray-300 dark:text-white text-gray-900  px-3 py-3 rounded-lg cursor-pointer hover:outline-indigo-700 hover:outline-1" onClick={() => setState(!state)}>
+        <span className="mr-2"><i className='bx bxs-book-open'></i>&nbsp;Docs</span>
+        <ul className={`absolute top-full left-0 mt-1 dark:bg-gray-900 dark:text-white text-gray-800 bg-gray-300 rounded-lg p-2 w-full ${DropdownClass} z-50 shadow-lg`}>
+          {DefaultDocs.map((doc, index) => (
+            <Links key={index} name={doc.name} url={doc.url} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,13 +1,13 @@
 import { UrlDomain } from "../constants/constants";
 
-export default function Links({ name, url }: { name: string, url: string }) {
-    const domain = new URL("https://seyfert.dev").origin;
+export default function Link({ name, url }: { name: string, url: string }) {
+    const domain = new URL(url).origin;
     const iconUrl = `${UrlDomain}${domain}`;
     return (
         <li className="w-full text-left p-1 rounded">
-            <a href="https://seyfert.dev/" rel="noopener noreferrer" className="flex items-center gap-2">
-                <img src={iconUrl} alt="" width={16} height={16} />
-                Seyfert
+            <a href={url} rel="noopener noreferrer" className="flex items-center gap-2">
+                <img src={iconUrl} alt="" width={16} height={16} className="rounded-sm"/>
+                {name}
             </a>
         </li>
     );
